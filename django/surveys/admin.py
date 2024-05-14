f6da01184f2a141589b9ed9f030b2057689c51ee
BE-02-Kimchangkyu <ckyu13@gmail.com>
2024-05-14 15:52:08 +0900

    surveys_

--- django/surveys/admin.py
+++ django/surveys/admin.py
@@ -7,9 +7,13 @@ class SurveyAnswerInline(admin.TabularInline):
     extra = 7
 
 
+class SurveyAnswerAdmin(admin.ModelAdmin):
+    fields = ["survey", "answer_choice", "member_id", "created_at", "updated_at"]
+
+
 class SurveyQuestionAdmin(admin.ModelAdmin):
     inlines = [SurveyAnswerInline]
 
 
 admin.site.register(SurveyQuestion, SurveyQuestionAdmin)
-admin.site.register(SurveyAnswer)
+admin.site.register(SurveyAnswer, SurveyAnswerAdmin)
